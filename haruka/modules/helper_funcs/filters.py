8fa0c7d31e84ba3c6dd76afc1ae7a35fa1c38e51
Chizuru <chizuru@kanojo.tk>
2021-01-08 14:18:07 +0530

    filters: use MessageFilter
    Co-Authored-By: Stɑrry Shivɑm <58552395+starry69@users.noreply.github.com>

--- haruka/modules/helper_funcs/filters.py
+++ haruka/modules/helper_funcs/filters.py
@@ -16,33 +16,38 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from telegram import Message
-from telegram.ext import BaseFilter
+from telegram.ext import MessageFilter
 
 from haruka import SUDO_USERS
 
 
 class CustomFilters(object):
-    class _Sudoers(BaseFilter):
+    class _Sudoers(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.from_user
-                        and message.from_user.id in SUDO_USERS)
+            return bool(message.from_user and message.from_user.id in SUDO_USERS)
 
     sudo_filter = _Sudoers()
 
-    class _MimeType(BaseFilter):
+    class _MimeType(MessageFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
 
         def filter(self, message: Message):
-            return bool(message.document
-                        and message.document.mime_type == self.mime_type)
+            return bool(
+                message.document and message.document.mime_type == self.mime_type
+            )
 
     mime_type = _MimeType
 
-    class _HasText(BaseFilter):
+    class _HasText(MessageFilter):
         def filter(self, message: Message):
-            return bool(message.text or message.sticker or message.photo
-                        or message.document or message.video)
+            return bool(
+                message.text
+                or message.sticker
+                or message.photo
+                or message.document
+                or message.video
+            )
 
     has_text = _HasText()
